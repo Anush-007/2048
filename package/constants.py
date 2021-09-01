76515e378326b4eeebfb4314d1cc166dc32b3a5f
Anush-007 <82879558+Anush-007@users.noreply.github.com>
2021-09-01 05:54:27 +0530

    Changed the constants mechanism to simply initialised variables rather than a singleton, Cause I was tired to make data secure now

--- package/constants.py
+++ package/constants.py
@@ -1,22 +1,12 @@
-def singleton(_class) :
-	instances = {}
-	def getInstance(*args, **kwargs):
-		if _class not in instances:
-			instances[_class] = _class(*args, **kwargs)
-		return instances[_class]
-	return getInstance
-
 screenSize = 800
 
-@singleton
-class Constants:
-	title = "2048"
+title = "2048"
 
-	index = 4 # x means x * x grid
+index = 4 # x means x * x grid
 
-	bsize =  screenSize // index
+bsize =  screenSize // index
 
-	size = bsize * index
+size = bsize * index
 
-	choices = [1, 2, 4]
-	probabilites = [0.6, 0.3, 0.1] # probabilities for various numbers to occur when a number is being spawned from choices
+choices = [1, 2, 4]
+probabilites = [0.6, 0.3, 0.1] # probabilities for various numbers to occur when a number is being spawned from choices
